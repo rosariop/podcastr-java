91493ff007483cab143fe06edb6a89df046f5b68
rosario polito <rosario.polito@gmx.de>
2022-09-03 09:53:50 +0200

    minor refactoring

--- src/main/java/de/podcastr/desktop/controller/PodcastListButtonAction.java
+++ src/main/java/de/podcastr/desktop/controller/PodcastListButtonAction.java
@@ -3,7 +3,7 @@ package de.podcastr.desktop.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class B1Action implements ActionListener {
+public class PodcastListButtonAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("works");
--- src/main/java/de/podcastr/desktop/view/Sidebar.java
+++ src/main/java/de/podcastr/desktop/view/Sidebar.java
@@ -1,6 +1,6 @@
 package de.podcastr.desktop.view;
 
-import de.podcastr.desktop.controller.B1Action;
+import de.podcastr.desktop.controller.PodcastListButtonAction;
 
 import javax.swing.*;
 
@@ -10,18 +10,6 @@ public class Sidebar extends JScrollPane {
 
     private static Sidebar instance;
 
-    public JScrollPane buildSidebar(JPanel panel){
-        JScrollPane scrollPane = new JScrollPane (panel,
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-        /*TODO: -> model -> datenbank -> load podcasts*/
-        JButton b1 = new JButton("Some Button");
-        b1.addActionListener(new B1Action());
-        panel.add(b1);
-        return scrollPane;
-    }
-
     public static Sidebar getInstance(){
         if (instance == null){
             instance = new Sidebar();
@@ -29,4 +17,15 @@ public class Sidebar extends JScrollPane {
         return instance;
     }
 
+    public JScrollPane buildSidebar(JPanel panel){
+        JScrollPane scrollPane = new JScrollPane (panel,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        /*TODO: -> model -> datenbank -> load podcasts*/
+        JButton b1 = new JButton("Some Button");
+        b1.addActionListener(new PodcastListButtonAction());
+        panel.add(b1);
+        return scrollPane;
+    }
 }
